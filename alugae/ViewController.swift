beab15484659e98bb2d85cc3bd7f12c8048277f6
Vitor Schiavo <vitorschiavo@MacBook-Pro-de-Vitor.local>
2018-10-21 01:03:00 -0300

    [alugae] - first commit

---
+++ alugae/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alugae
+//
+//  Created by Vitor Schiavo on 21/10/2018.
+//  Copyright © 2018 alugae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
